c8518bbcb9d6d157fd476fa08a33da851a477fc7
alta189 <stephendwilliams7@gmail.com>
2011-08-05 02:10:05 -0400

    Need to dispose on close not exit on close

--- src/main/java/org/spoutcraft/launcher/GUI/LoginForm.java
+++ src/main/java/org/spoutcraft/launcher/GUI/LoginForm.java
@@ -141,7 +141,7 @@ public class LoginForm extends JFrame implements ActionListener {
         });
 		
 		setTitle("Spoutcraft Launcher");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 861, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
