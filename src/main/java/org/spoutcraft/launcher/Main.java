1263b4723c68595b96eeccac866790fc463ffaf1
K900 <k0009000@gmail.com>
2011-09-24 18:44:29 +0400

    Fix portable mode (again).

--- src/main/java/org/spoutcraft/launcher/Main.java
+++ src/main/java/org/spoutcraft/launcher/Main.java
@@ -74,7 +74,11 @@ public class Main {
 			recursion.createNewFile();
 			reboot("-Xmx" + mem + "m");
 		}
-		
+
+        if (Arrays.asList(args).contains("--portable") || new File("spoutcraft-portable").exists()) {
+            PlatformUtils.setPortable(true);
+        }
+
 		PlatformUtils.getWorkingDirectory().mkdirs();
 
 		new File(PlatformUtils.getWorkingDirectory(), "spoutcraft").mkdir();
@@ -93,38 +97,22 @@ public class Main {
 
 		switch (args.length) {
 		case 4:
-			if (Arrays.asList(args).contains("--portable") || new File("spoutcraft-portable").exists()) {
-				PlatformUtils.setPortable(true);
-			}
-
 			login.doLogin(args[0], args[1]);
 
 			MinecraftUtils.setServer(args[2]);
 			break;
 
 		case 3:
-			if (Arrays.asList(args).contains("--portable") || new File("spoutcraft-portable").exists()) {
-				PlatformUtils.setPortable(true);
-			}
-
 			login.doLogin(args[0], args[1]);
 
 			MinecraftUtils.setServer(args[2]);
 			break;
 		case 2:
-			if (Arrays.asList(args).contains("--portable") || new File("spoutcraft-portable").exists()) {
-				PlatformUtils.setPortable(true);
-			}
-
 			login.doLogin(args[0], args[1]);
 
 			break;
 		default:
 			if (args.length > 5) {
-				if (Arrays.asList(args).contains("--portable") || new File("spoutcraft-portable").exists()) {
-					PlatformUtils.setPortable(true);
-				}
-
 				MinecraftUtils.setServer(args[2]);
 			}
 		}
