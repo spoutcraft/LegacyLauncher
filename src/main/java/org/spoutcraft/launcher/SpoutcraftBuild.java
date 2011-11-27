64b0f94b9b0f4aafda3a23dd8cbdd2a6cc1851f7
alta189 <stephendwilliams7@gmail.com>
2011-11-27 03:41:15 -0500

    Fix cast error

--- src/main/java/org/spoutcraft/launcher/SpoutcraftBuild.java
+++ src/main/java/org/spoutcraft/launcher/SpoutcraftBuild.java
@@ -10,42 +10,43 @@ public class SpoutcraftBuild {
 	private String latestVersion;
 	private int build;
 	private DownloadListener listener = null;
+
 	public SpoutcraftBuild(String minecraft, String latest, int build) {
 		this.minecraftVersion = minecraft;
 		this.latestVersion = latest;
 		this.build = build;
-		
+
 	}
-	
+
 	public int getBuild() {
 		return build;
 	}
-	
+
 	public String getMinecraftVersion() {
 		return minecraftVersion;
 	}
-	
+
 	public String getLatestMinecraftVersion() {
 		return latestVersion;
 	}
-	
+
 	public String getMinecraftURL(String user) {
 		return "http://s3.amazonaws.com/MinecraftDownload/minecraft.jar?user=" + user + "&ticket=1";
 	}
-	
+
 	public String getSpoutcraftURL() {
 		return MirrorUtils.getMirrorUrl("Spoutcraft/" + build + "/spoutcraft-dev-SNAPSHOT.jar", null, listener);
 	}
-	
+
 	public void setDownloadListener(DownloadListener listener) {
 		this.listener = listener;
 	}
-	
+
 	public void install() {
 		Configuration config = SpoutcraftYML.getSpoutcraftYML();
 		config.setProperty("current", getBuild());
 	}
-	
+
 	public int getInstalledBuild() {
 		Configuration config = SpoutcraftYML.getSpoutcraftYML();
 		return config.getInt("current", -1);
@@ -60,20 +61,22 @@ public class SpoutcraftBuild {
 		fallbackURL += "-" + getMinecraftVersion() + ".patch";
 		return MirrorUtils.getMirrorUrl(mirrorURL, fallbackURL, null);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static SpoutcraftBuild getSpoutcraftBuild() {
 		Configuration config = SpoutcraftYML.getSpoutcraftYML();
-		Map<Integer, String> builds = (Map<Integer, String>) config.getProperty("builds");
+		Map<Integer, Object> builds = (Map<Integer, Object>) config.getProperty("builds");
 		int latest = config.getInt("latest", -1);
 		int recommended = config.getInt("recommended", -1);
 		int selected = SettingsUtil.getSelectedBuild();
 		if (SettingsUtil.isRecommendedBuild()) {
-			return new SpoutcraftBuild(builds.get(recommended), "1.0.0", recommended);
+			Map<String, Object> build = (Map<String, Object>) builds.get(recommended);
+			return new SpoutcraftBuild((String)build.get("minecraft"), "1.0.0", recommended);
+		} else if (SettingsUtil.isDevelopmentBuild()) {
+			Map<String, Object> build = (Map<String, Object>) builds.get(recommended);
+			return new SpoutcraftBuild((String)build.get("minecraft"), "1.0.0", latest);
 		}
-		else if (SettingsUtil.isDevelopmentBuild()) {
-			return new SpoutcraftBuild(builds.get(latest), "1.0.0", latest);
-		}
-		return new SpoutcraftBuild(builds.get(selected), "1.0.0", selected);
+		Map<String, Object> build = (Map<String, Object>) builds.get(recommended);
+		return new SpoutcraftBuild((String)build.get("minecraft"), "1.0.0", selected);
 	}
 }
