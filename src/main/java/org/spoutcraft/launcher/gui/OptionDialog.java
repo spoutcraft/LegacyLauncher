d4b5eb157b8646c5f2c0990ef4508609472bc496
Afforess <afforess@gmail.com>
2012-01-14 16:47:25 -0500

    import java.awt.Dialog.ModalityType;

--- src/main/java/org/spoutcraft/launcher/gui/OptionDialog.java
+++ src/main/java/org/spoutcraft/launcher/gui/OptionDialog.java
@@ -21,6 +21,7 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dialog.ModalityType;
 import java.io.IOException;
 
 import javax.swing.JButton;
