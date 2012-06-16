bb566e4b7b9d713d5a6dbf850e778cbd6c359dc7
Afforess <afforess@gmail.com>
2012-06-16 18:55:02 -0400

    Cleanup up Resources.java

--- src/main/java/org/spoutcraft/launcher/yml/Resources.java
+++ src/main/java/org/spoutcraft/launcher/yml/Resources.java
@@ -7,128 +7,48 @@ import org.spoutcraft.launcher.api.util.Utils;
 import org.spoutcraft.launcher.api.util.YAMLProcessor;
 
 public enum Resources implements YAMLResource{
-	Launcher {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/launcher.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "launcher.yml"),
-				null);
+	Launcher ("http://get.spout.org/launcher.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "launcher.yml"),
+			null),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
-	Libraries {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/libraries.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "libraries.yml"),
-				null);
+	Libraries ("http://get.spout.org/libraries.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "libraries.yml"),
+			null),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
-	Minecraft {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/minecraft.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "minecraft.yml"),
-				new PersistCurrentResourceAction());
+	Minecraft ("http://get.spout.org/minecraft.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "minecraft.yml"),
+			new PersistCurrentResourceAction()),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	}, 
-	Special {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/special.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "special.yml"),
-				null);
+	Special ("http://get.spout.org/special.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "special.yml"),
+			null),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
-	VIP {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/vip.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "vip.yml"),
-				null);
+	VIP ("http://get.spout.org/vip.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "vip.yml"),
+			null),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
-	Spoutcraft {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/spoutcraft.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "spoutcraft.yml"),
-				new PersistCurrentResourceAction());
+	Spoutcraft ("http://get.spout.org/spoutcraft.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "spoutcraft.yml"),
+			new PersistCurrentResourceAction()),
 
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
-	Assets {
-		final BaseYAMLResource resource = new BaseYAMLResource(
-				"http://get.spout.org/assets.yml",
-				new File(Utils.getWorkingDirectory(), "config" + File.separator + "assets.yml"),
-				null);
-
-		@Override
-		public YAMLProcessor getYAML() {
-			return resource.getYAML();
-		}
-		
-		@Override
-		public boolean updateYAML() {
-			return resource.updateYAML();
-		}
-	},
+	Assets ("http://get.spout.org/assets.yml",
+			new File(Utils.getWorkingDirectory(), "config" + File.separator + "assets.yml"),
+			null),
 	;
 
+	final BaseYAMLResource resource;
+	private Resources(String url, File directory, ResourceAction action) {
+		this.resource = new BaseYAMLResource(url, directory, action);
+	}
+
 	public YAMLProcessor getYAML() {
-		throw new UnsupportedOperationException("Not supported");
+		return resource.getYAML();
 	}
 
 	public boolean updateYAML() {
-		throw new UnsupportedOperationException("Not supported");
+		return resource.updateYAML();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static String getLibraryMD5(String library, String version) {
 		YAMLProcessor config = Resources.Libraries.getYAML();
@@ -142,7 +62,7 @@ public enum Resources implements YAMLResource{
 		}
 		return result;
 	}
-	
+
 	public static String getLatestMinecraftVersion() {
 		return Resources.Minecraft.getYAML().getString("latest");
 	}
@@ -160,20 +80,19 @@ public enum Resources implements YAMLResource{
 	public static String getInstalledVersion() {
 		return Resources.Minecraft.getYAML().getString("current");
 	}
+}
 
-	private class PersistCurrentResourceAction implements ResourceAction {
-		String previousValue = null;
+class PersistCurrentResourceAction implements ResourceAction {
+	String previousValue = null;
 
-		public void beforeAction(YAMLProcessor previous) {
-			previousValue = previous.getString("current", null);
-		}
-
-		public void afterAction(YAMLProcessor current) {
-			current.setProperty("current", previousValue);
-			current.save();
-			previousValue = null;
-		}
-		
+	public void beforeAction(YAMLProcessor previous) {
+		previousValue = previous.getString("current", null);
 	}
 
+	public void afterAction(YAMLProcessor current) {
+		current.setProperty("current", previousValue);
+		current.save();
+		previousValue = null;
+	}
+	
 }
