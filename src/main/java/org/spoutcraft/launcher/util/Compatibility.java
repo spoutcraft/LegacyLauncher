fc4618f8e4ab20789b7fc12cb5cf2b5466a020c3
Chris Sanders <zidane@outlook.com>
2012-12-09 15:37:48 -0600

    Forgot to set param as File type. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spoutcraft/launcher/util/Compatibility.java
+++ src/main/java/org/spoutcraft/launcher/util/Compatibility.java
@@ -57,7 +57,7 @@ public class Compatibility {
 	public static void open(File file) {
 		try {
 			Object o = Class.forName("java.awt.Desktop").getMethod("getDesktop", new Class[0]).invoke(null, new Object[0]);
-			o.getClass().getMethod("open", new Class[]{URI.class}).invoke(o, new Object[]{file});
+			o.getClass().getMethod("open", new Class[]{File.class}).invoke(o, new Object[]{file});
 		} catch (Exception e) {
 			if (Settings.isDebugMode()) {
 				e.printStackTrace();
