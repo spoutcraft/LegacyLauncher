ec3162c3c35a91435ed807862ddc5298daa3ad62
Dalton Pelc <daltonjpelc@gmail.com>
2013-01-16 17:36:18 -0500

    Added modpack.yml URL, and md5 checking
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/main/java/org/spoutcraft/launcher/technic/TechnicModpack.java
+++ src/main/java/org/spoutcraft/launcher/technic/TechnicModpack.java
@@ -1,4 +1,21 @@
 package org.spoutcraft.launcher.technic;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.spoutcraft.launcher.api.Launcher;
+import org.spoutcraft.launcher.exceptions.DownloadException;
+import org.spoutcraft.launcher.util.DownloadListener;
+import org.spoutcraft.launcher.util.DownloadUtils;
+
 public class TechnicModpack extends Modpack {
+
+	public static void checkForUpdates(String modpack, DownloadListener listener) throws DownloadException {
+		File location = new File(Launcher.getGameUpdater().getTempDir(), "modpack.yml");
+		try {
+			DownloadUtils.downloadFile(TechnicRestAPI.getModpackYMLURL(modpack), location.getPath(), location.getName(), TechnicRestAPI.getModpackMD5(modpack), listener);
+		} catch (IOException e) {
+			throw new DownloadException(e);
+		}
+	}
 }
--- src/main/java/org/spoutcraft/launcher/technic/TechnicRestAPI.java
+++ src/main/java/org/spoutcraft/launcher/technic/TechnicRestAPI.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.annotate.JsonProperty;
@@ -21,6 +22,7 @@ public class TechnicRestAPI {
 	public static final String MODPACKS_URL = REST_URL + "modpacks/";
 	public static final String CACHE_URL = REST_URL + "cache/";
 	public static final String MOD_URL = CACHE_URL + "mod/";
+	public static final String MIRROR_URL = "http://mirror.technicpack.net/Technic/";
 
 	public static String getModDownloadURL(String mod, String build) {
 		return CACHE_URL + mod + "/" + build;
@@ -34,18 +36,26 @@ public class TechnicRestAPI {
 		return MODPACKS_URL + modpack + "/build/" + build;
 	}
 
+	public static String getModpackMD5URL(String modpack) {
+		return MODPACKS_URL + modpack + "/MD5";
+	}
+
+	public static String getModpackYMLURL(String modpack) {
+		return MIRROR_URL + modpack + "/modpack.yml";
+	}
+
 	public static String getModpackBuildsURL(String modpack) {
 		return MODPACKS_URL + modpack;
 	}
 
-	public static Modpacks getModpacks() throws RestfulAPIException {
+	public static List<String> getModpacks() throws RestfulAPIException {
 		InputStream stream = null;
 		String url = MODPACKS_URL;
 		try {
 			URL conn = new URL(url);
 			stream = conn.openConnection().getInputStream();
 			Modpacks result = mapper.readValue(stream, Modpacks.class);
-			return result;
+			return result.getModpacks();
 		} catch (IOException e) {
 			throw new RestfulAPIException("Error accessing URL [" + url + "]", e);
 		} finally {
@@ -99,6 +109,21 @@ public class TechnicRestAPI {
 			stream = conn.openConnection().getInputStream();
 			ModpackBuilds result = mapper.readValue(stream, ModpackBuilds.class);
 			return result;
+		} catch (IOException e) {
+			throw new RestfulAPIException("Error accessing URL [" + url + "]", e);
+		} finally {
+			IOUtils.closeQuietly(stream);
+		}
+	}
+
+	public static String getModpackMD5(String modpack) throws RestfulAPIException {
+		InputStream stream = null;
+		String url = getModpackMD5URL(modpack);
+		try {
+			URL conn = new URL(url);
+			stream = conn.openConnection().getInputStream();
+			TechnicMD5 md5Result = mapper.readValue(stream, TechnicMD5.class);
+			return md5Result.getMD5();
 		} catch (IOException e) {
 			throw new RestfulAPIException("Error accessing URL [" + url + "]", e);
 		} finally {
