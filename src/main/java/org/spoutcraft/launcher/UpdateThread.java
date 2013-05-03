1219950a5afbfb8292958c744b309ad42391656c
Wulfspider <hello@wulfspider.com>
2013-05-03 04:14:26 -0400

    Use new Minecraft download location.

--- src/main/java/org/spoutcraft/launcher/UpdateThread.java
+++ src/main/java/org/spoutcraft/launcher/UpdateThread.java
@@ -432,7 +432,7 @@ public class UpdateThread extends Thread {
 		logger.info("Minecraft Version: " + build.getMinecraft());
 		File mcCache = new File(Launcher.getGameUpdater().getBinCacheDir(), "minecraft_" + minecraft.getVersion() + ".jar");
 		if (!mcCache.exists() || (minecraftMD5 == null || !minecraftMD5.equals(MD5Utils.getMD5(mcCache)))) {
-			DownloadUtils.downloadFile("http://assets.minecraft.net/" + minecraft.getVersion().replaceAll("\\.", "_") + "/minecraft.jar", mcCache.getPath(), null, minecraftMD5, listener);
+			DownloadUtils.downloadFile("https://s3.amazonaws.com/Minecraft.Download/versions/" + minecraft.getVersion() + "/" + minecraft.getVersion() + ".jar", mcCache.getPath(), null, minecraftMD5, listener);
 		}
 		Utils.copy(mcCache, new File(Launcher.getGameUpdater().getBinDir(), "minecraft.jar"));
 
