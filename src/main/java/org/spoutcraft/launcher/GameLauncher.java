b2b218a3f9a0be144df67205e13d605bfdd5a209
Afforess <afforess@gmail.com>
2013-05-08 14:04:44 -0400

    Pass MC applet working directory path

--- src/main/java/org/spoutcraft/launcher/GameLauncher.java
+++ src/main/java/org/spoutcraft/launcher/GameLauncher.java
@@ -109,6 +109,7 @@ public class GameLauncher extends JFrame implements WindowListener {
 		minecraft.addParameter("downloadticket", downloadTicket);
 		minecraft.addParameter("spoutcraftlauncher", "true");
 		minecraft.addParameter("portable", params.isPortable() + "");
+		minecraft.addParameter("working_directory", Utils.getWorkingDirectory().getAbsolutePath());
 		if (params.getServer() != null) {
 			minecraft.addParameter("server", params.getServer());
 			if (params.getPort() != null) {
