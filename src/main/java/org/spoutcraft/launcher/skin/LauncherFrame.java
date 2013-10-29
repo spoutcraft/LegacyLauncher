a4c870a6b06b843d7e4957d3b9b424ca26dfa9c1
unknown <sdbaynham@gmail.com>
2013-10-29 01:28:52 -0500

    Trying again on lining up those texts - moved logout 3 pixels to the right

--- src/main/java/org/spoutcraft/launcher/skin/LauncherFrame.java
+++ src/main/java/org/spoutcraft/launcher/skin/LauncherFrame.java
@@ -156,7 +156,7 @@ public class LauncherFrame extends JFrame implements ActionListener, KeyListener
 		logout.setHorizontalAlignment(SwingConstants.RIGHT);
 		logout.setHorizontalTextPosition(SwingConstants.RIGHT);
 		logout.setForeground(Color.white);
-		logout.setBounds(userArea.getX() + 130, userArea.getY() + 32, 60, 30);
+		logout.setBounds(userArea.getX() + 133, userArea.getY() + 32, 60, 30);
 		logout.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		logout.setActionCommand(LOGOUT);
 		logout.addActionListener(this);
